60dc773b4065104f053c04e01b29aa720e081488
shikha-sarraf <sarrafshikha1@.com>
2026-06-16 10:52:27 +0545

    Add CV and animations

---
+++ script.js
@@ -0,0 +1,13 @@
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add('visible');
+    } else {
+      entry.target.classList.remove('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+document.querySelectorAll('.fade-in').forEach(section => {
+  observer.observe(section);
+});
